36faa32e36498e7864121270ed5a33f258be76ff
Muhammad Azam <AzamWahid@outlook.com>
2023-03-31 15:52:52 +0500

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+class Vehicle {
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+abstract class Vehicle {
+    make!: string;
+    mode!: string;
+    year!: number;
+    rented!: boolean;
+}
